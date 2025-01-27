d9a7e6f197347c03972936708915d91bdbefcf57
rekunolya <rekun.olya@gmail.com>
2025-01-27 19:19:47 +0300

    feat: add solution to task mergeObjects

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -33,8 +33,19 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const calcArray = {};
+  objects.forEach((elem) => {
+    Object.entries(elem).map(([key, value]) => {
+      if (calcArray[key] !== undefined) {
+        calcArray[key] += value;
+      } else {
+        calcArray[key] = value;
+      }
+      return key;
+    });
+  });
+  return calcArray;
 }
 
 /**
