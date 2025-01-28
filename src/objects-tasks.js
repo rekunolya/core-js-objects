46e74e66fa3c5d71d1611f06e1aa6631284e415d
rekunolya <rekun.olya@gmail.com>
2025-01-29 00:42:11 +0300

    feat: add solution to task Rectangle

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -175,8 +175,18 @@ function sellTickets(/* queue */) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  class RectangleTask {
+    constructor(widthConstr, heightConstr) {
+      this.width = widthConstr;
+      this.height = heightConstr;
+    }
+
+    getArea() {
+      return this.width * this.height;
+    }
+  }
+  return new RectangleTask(width, height);
 }
 
 /**
