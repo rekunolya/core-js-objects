bf5b7ad2fb2895002b361d8acc4b4a9f921d5102
rekunolya <rekun.olya@gmail.com>
2025-01-28 23:48:24 +0300

    feat: add solution to task makeWord

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -132,8 +132,16 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const values = Object.values(lettersObject).flat(Infinity);
+  const string = new Array(values.length);
+  Object.entries(lettersObject).map(([key, value]) => {
+    value.forEach((el) => {
+      string[el] = key;
+    });
+    return string;
+  });
+  return string.join('');
 }
 
 /**
