4f45beee57e9150ee2312a8d67d7cf8da975e776
rekunolya <rekun.olya@gmail.com>
2025-01-29 14:51:06 +0300

    feat: add a solution to task fromJSON

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -214,8 +214,10 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const object = JSON.parse(json);
+  const values = Object.values(object);
+  return new proto.constructor(...values);
 }
 
 /**
